26086bd36617877aeba281947bfadd5cb5ce53e1
Lochie Axon <lochieaxon@gmail.com>
2026-09-05 04:44:03 +1000

    cleanup

--- site/src/components/header/index.tsx
+++ site/src/components/header/index.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import styles from "./styles.module.scss";
 
 export const Header = () => {
   return (
     <div className={styles.header}>
       <div className={styles.left}>
-        <h1>&lt;TextMorph /&gt;</h1>
+        <Link href="/">
+          <h1>&lt;TextMorph /&gt;</h1>
+        </Link>
         <p>Text continuity for interfaces on the web.</p>
       </div>
     </div>
